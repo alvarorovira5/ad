8dcf7b2c3e0fa826e7dbabf84755db946e62076b
alvaro rovira <roviraibanezalvaro@gmail.com>
2016-01-12 10:06:22 +0100

    Con JOption Pane

--- GConection/src/main/java/GConection.java
+++ GConection/src/main/java/GConection.java
@@ -1,32 +1,31 @@
 import java.sql.*;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 public class GConection {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		String cad;
-		int op;
-		
-		
-		System.out.println("Elige una opcion" ); 
-		System.out.println("0.- Salir" ); 
-		System.out.println("1.- Leer" ); //nos pide el id del articulo y se muestran todos los articulos con ese id
-		System.out.println("2.- Nuevo" ); // crear nuevo articulo, con todos sus campos, "nombre,precio,categoria" insert
-		System.out.println("3.- Editar" ); // hacer un update de un articulo con los campos a modificar
-		System.out.println("4.- Eliminar" );  //pide el id y se elimina
-		System.out.println("5.- Listar Todos" ); 
-		
+		int op=0;
 		do{
+			
+			op =Integer.parseInt(JOptionPane.showInputDialog(""
+					+ "Elige una opcion\n"
+					+ "0.- Salir\n"
+					+ "1.- Leer\n"
+					+ "2.- Nuevo\n"
+					+ "3.- Editar\n"
+					+ "4.- Eliminar\n"
+					+ "5.- Listar Todos\n"));
+
 		
-		
-				//System.out.println("Operación: "+op);
-				cad=teclado.nextLine();
-				op=Integer.parseInt(cad);
+//				op = Integer.parseInt(teclado.nextLine());
 				
-				switch (op) {
+				
+			switch (op) {
 				
 		       
 		        case 1:  System.out.println("Caso 1");
@@ -52,60 +51,77 @@ public class GConection {
 		} while(op != 0);
 			
 	}
-	public static void eliminar() throws SQLException{
+	
+	public static Connection connect() throws SQLException{
 		Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/dbprueba","root","");
 		System.out.println("Conexion");
+		
+		return connection;
+		
+	}
+	public static void eliminar() throws SQLException{
+		//Connection connection = DriverManager.getConnection(
+			//	"jdbc:mysql://localhost/dbprueba","root","");
+//		System.out.println("Conexion");
 		
 		Scanner teclado = new Scanner(System.in);
 		String cad;
 		
-		System.out.println("Dime la id del producto que quieres eliminar: ");
+		//System.out.println("Dime la id del producto que quieres eliminar: ");
 		int id;
 		
-		cad=teclado.nextLine();
-		id=Integer.parseInt(cad);
+//		cad=teclado.nextLine();
+//		id=Integer.parseInt(cad);
 		
+		id=Integer.parseInt(JOptionPane.showInputDialog("Dime la id del producto que quieres eliminar: "));
 		
 		String query="DELETE FROM articulos WHERE id="+id;
-		PreparedStatement preparedStmt = connection.prepareStatement(query);
+		PreparedStatement preparedStmt = connect().prepareStatement(query);
 		preparedStmt.execute();
 		
-		connection.close();
+		connect().close();
 		
 	}
 	public static void editar() throws SQLException{
-		//por hacer...
+		
 		
 		//System.out.println("Estoy editando.");
 		
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba","root","");
-		System.out.println("Conexion");
+//		Connection connection = DriverManager.getConnection(
+//				"jdbc:mysql://localhost/dbprueba","root","");
+//		System.out.println("Conexion");
 		
 		Scanner teclado = new Scanner(System.in);
 		String cad,nombre,categoria;
 		float precio;
 		
 		int id;
-		System.out.println("Dime la id del producto que quieres actualizar: ");
-		cad=teclado.nextLine();
-		id=Integer.parseInt(cad);
+//		System.out.println("Dime la id del producto que quieres actualizar: ");
+//		cad=teclado.nextLine();
+//		id=Integer.parseInt(cad);
 		
-		System.out.println("Nuevo nombre del articulo: ");
-		nombre=teclado.nextLine();
+		id=Integer.parseInt(JOptionPane.showInputDialog("Dime la id del producto que quieres actualizar: "));
 		
-		System.out.println("Nuevo precio del articulo: ");
-		cad=teclado.nextLine();
-		precio=Float.parseFloat(cad);
+//		System.out.println("Nuevo nombre del articulo: ");
+//		nombre=teclado.nextLine();
 		
-		System.out.println("Nueva categoria del articulo: ");
-		categoria=teclado.nextLine();
+		nombre=JOptionPane.showInputDialog("Nuevo nombre del articulo: ");
+		
+//		System.out.println("Nuevo precio del articulo: ");
+//		cad=teclado.nextLine();
+//		precio=Float.parseFloat(cad);
+		
+		precio=Float.parseFloat(JOptionPane.showInputDialog("Nuevo precio del articulo: "));
+		
+//		System.out.println("Nueva categoria del articulo: ");
+//		categoria=teclado.nextLine();
+		categoria=JOptionPane.showInputDialog("Nueva categoria del articulo: ");
 		String query="UPDATE articulos "
 				+    "SET nombre=?, precio=?, categoria=?"
 				+    "WHERE id="+id;
 		
-		PreparedStatement preparedStmt = connection.prepareStatement(query);
+		PreparedStatement preparedStmt = connect().prepareStatement(query);
 		
 		preparedStmt.setString (1, nombre);
 		preparedStmt.setInt(2, (int) precio);
@@ -113,15 +129,15 @@ public class GConection {
 		
 		preparedStmt.execute();
 		
-		connection.close();
+		connect().close();
 		
 		
 	}
 	
 	public static void nuevo() throws SQLException{
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba","root","");
-		System.out.println("Conexion");
+//		Connection connection = DriverManager.getConnection(
+//				"jdbc:mysql://localhost/dbprueba","root","");
+//		System.out.println("Conexion");
 		
 		Scanner teclado = new Scanner(System.in);
 		String cad;
@@ -130,20 +146,26 @@ public class GConection {
 		String nombre,categoria;
 		float precio;
 		
-		System.out.println("Introduce el nombre del nuevo articulo: ");
-		nombre=teclado.nextLine();
+//		System.out.println("Introduce el nombre del nuevo articulo: ");
+//		nombre=teclado.nextLine();
 		
-		System.out.println("Introduce el precio del nuevo articulo: ");
-		cad=teclado.nextLine();
-		precio=Float.parseFloat(cad);
+		nombre=JOptionPane.showInputDialog("Introduce el nombre del nuevo articulo: ");
 		
-		System.out.println("Introduce la categoria del nuevo articulo ");
-		categoria=teclado.nextLine();
+//		System.out.println("Introduce el precio del nuevo articulo: ");
+//		cad=teclado.nextLine();
+//		precio=Float.parseFloat(cad);
+		
+		precio=Float.parseFloat(JOptionPane.showInputDialog("Introduce el precio del nuevo articulo: "));
+		
+//		System.out.println("Introduce la categoria del nuevo articulo ");
+//		categoria=teclado.nextLine();
+		
+		categoria=JOptionPane.showInputDialog("Introduce la categoria del nuevo articulo ");
 		
 		
 		String query="INSERT INTO articulos(nombre,precio,categoria) VALUES(?,?,?)";
 		
-		PreparedStatement preparedStmt = connection.prepareStatement(query);
+		PreparedStatement preparedStmt = connect().prepareStatement(query);
 		preparedStmt.setString (1, nombre);
 		preparedStmt.setInt(2, (int) precio);
 		preparedStmt.setString (3, categoria);
@@ -156,23 +178,26 @@ public class GConection {
 				
 		
 		*/
-		connection.close();
+		connect().close();
 	}
 	
 	public static void listarTodos() throws SQLException{
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba","root","");
-		System.out.println("Conexion");
+//		Connection connection = DriverManager.getConnection(
+//				"jdbc:mysql://localhost/dbprueba","root","");
+//		System.out.println("Conexion");
 		
 		//SELECT
 		
-		Statement statement = (Statement) connection.createStatement();
+		Statement statement = (Statement) connect().createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM articulos");
+		String resultado="";
 		while (resultSet.next()) {
-			System.out.println(resultSet.getString("nombre"));
+			//System.out.println(resultSet.getString("nombre"));
+			resultado+=resultSet.getString("nombre")+"\n";
 		}
+		JOptionPane.showMessageDialog(null, resultado);
 				
-		connection.close();
+		connect().close();
 		
 	}
 	
@@ -183,23 +208,28 @@ public class GConection {
 		String cad;
 		int id;
 				
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/dbprueba","root","");
-		System.out.println("Conexion");
+//		Connection connection = DriverManager.getConnection(
+//				"jdbc:mysql://localhost/dbprueba","root","");
+//		System.out.println("Conexion");
 		
-		System.out.println("¿Que artículo quieres? Danos su id: ");
-		cad=teclado.nextLine();
-		id=Integer.parseInt(cad);
+		//System.out.println("¿Que artículo quieres? Danos su id: ");
+		//cad=teclado.nextLine();
+		id=Integer.parseInt(JOptionPane.showInputDialog("¿Que artículo quieres? Danos su id: "));
 		
 		
-		Statement statement = (Statement) connection.createStatement();
+		
+		Statement statement = (Statement) connect().createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT nombre,precio,categoria FROM articulos WHERE id="+id);
+		String resultado="";
 		while (resultSet.next()) {
-			System.out.println(resultSet.getString("nombre"));
-			System.out.println(resultSet.getString("precio"));
-			System.out.println(resultSet.getString("categoria"));
+//			System.out.println(resultSet.getString("nombre"));
+//			System.out.println(resultSet.getString("precio"));
+//			System.out.println(resultSet.getString("categoria"));
+			resultado+=resultSet.getString("nombre")+" "+resultSet.getString("precio")+" "+resultSet.getString("categoria");
 		}
 		
-		connection.close();
+		JOptionPane.showMessageDialog(null, resultado);
+		
+		connect().close();
 	}
 }
