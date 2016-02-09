953b1c86dfcc43191319d16e557693fdb719b655
alvaro rovira <roviraibanezalvaro@gmail.com>
2016-02-09 10:41:17 +0100

    class Pedido

--- GPedido/src/main/java/org/institutoserpis/ad/Pedido.java
+++ GPedido/src/main/java/org/institutoserpis/ad/Pedido.java
@@ -1,5 +1,49 @@
 package org.institutoserpis.ad;
 
-public class Pedido {
+import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Pedido {
+	private long id;
+	private Cliente cliente;
+	private Calendar fecha;
+	
+
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Calendar getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Calendar fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	
+	
 }
